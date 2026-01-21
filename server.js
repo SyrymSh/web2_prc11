4ae24f158e44b32fa8bd939bf4836d460421ae5e
Syrym Shadiyarbek <syrym.shadiyarbek@gmail.com>
2026-01-21 16:57:32 +0500

    Ready for hosting

--- server.js
+++ server.js
@@ -5,23 +5,20 @@ import Item from "./models/Item.js";
 
 dotenv.config();
 
-/* 1️⃣ СОЗДАЁМ APP */
+
 const app = express();
 
-/* 2️⃣ MIDDLEWARE */
+
 app.use(express.json());
 
-/* 3️⃣ MONGODB */
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Atlas connected"))
   .catch(err => console.error("MongoDB error:", err));
 
-/* 4️⃣ ROUTES */
 app.get("/", (req, res) => {
   res.json({ message: "API works" });
 });
 
-/* CREATE */
 app.post("/api/items", async (req, res) => {
   try {
     const item = new Item(req.body);
@@ -32,20 +29,17 @@ app.post("/api/items", async (req, res) => {
   }
 });
 
-/* READ ALL */
 app.get("/api/items", async (req, res) => {
   const items = await Item.find();
   res.json(items);
 });
 
-/* READ ONE */
 app.get("/api/items/:id", async (req, res) => {
   const item = await Item.findById(req.params.id);
   if (!item) return res.status(404).json({ message: "Not found" });
   res.json(item);
 });
 
-/* UPDATE */
 app.put("/api/items/:id", async (req, res) => {
   const updated = await Item.findByIdAndUpdate(
     req.params.id,
@@ -55,14 +49,12 @@ app.put("/api/items/:id", async (req, res) => {
   res.json(updated);
 });
 
-/* DELETE */
 app.delete("/api/items/:id", async (req, res) => {
   await Item.findByIdAndDelete(req.params.id);
   res.json({ message: "Deleted" });
 });
 
-/* 5️⃣ START SERVER */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3011;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
